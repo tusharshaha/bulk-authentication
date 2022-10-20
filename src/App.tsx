f9b62995393e627b15efcafa29589125de24d7f0
tusharshaha <tusharshaha128@gmail.com>
2022-10-20 12:53:45 +0600

    created route setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-function App() {
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import ProtectedRoute from './Routes/ProtectedRoute';
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      
-    </div>
-  );
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<ProtectedRoute />}>
+          <Route path='' element={<Home />} />
+        </Route>
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  )
+};
 
 export default App;
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound: React.FC = () => {
+    return (
+        <div>
+            <h3>404 | Page not found!</h3>
+        </div>
+    );
+};
+
+export default NotFound;
